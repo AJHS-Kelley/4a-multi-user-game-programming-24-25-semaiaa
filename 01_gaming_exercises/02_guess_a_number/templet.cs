03cdacbda7c74da9ba19fbd6e54586ea73c32ae0
semaiaa <maialei06@icloud.com>
2024-09-06 13:57:29 -0400

    gamingexersises

---
+++ 01_gaming_exercises/02_guess_a_number/templet.cs
@@ -0,0 +1,22 @@
+using System;
+class GuessNumber {
+  static void Main() {
+  int numGuess = 0; 
+  int maxGuess = 8; 
+
+  // Generate the secret number here. 
+  // while loop
+      // Allow the user to guess the munber. 
+    // Compare the guess to the secret number.
+     // If match, print win, if > or <, give a hint. 
+     // +1 to numGuess
+     // Check if numGuess > maxGuesses. 
+     // If true, print a "lose game" message, if false guess again.
+
+
+
+  }
+  }
+  
+
+    
