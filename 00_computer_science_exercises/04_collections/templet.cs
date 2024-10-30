58b5c3d3300dfa23fccb35343693d824756db449
semaiaa <maialei06@icloud.com>
2024-10-30 13:03:49 -0400

    Update templet.cs

--- 00_computer_science_exercises/04_collections/templet.cs
+++ 00_computer_science_exercises/04_collections/templet.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 
 using System;
-class T04_Collections {
+class Collections {
   static void Main() { // DO NOT DELETE
   /*
   In C# a collection is known as an ARRAY. 
@@ -18,10 +18,29 @@ class T04_Collections {
   */
 
   // DECLARING AND DEFINING AN ARRAY
-  string[] breakfastFoods = {"Bacon", "Waffals", "Sausage", "Eggs" "Grits"};
+  string [] breakFastFoods = {"Bacon", "Waffals", "Sausage", "Eggs" "Grits"};
   int[] testScore = {0, 25, 15, 75, 99};
   double[]GPA = {0.27D, 1,99D, 4.02D, 0.0D, 3.25D};
-  
+
+  // Print Array Contents -- All Elements on Single Line
+  Console.WriteLine("The elements of each array are:\n"); 
+  Console.WriteLine("breakFastFoods: \n" + String.Join(",", breakfastFoods));
+  Console.WriteLine(); // Print an empty line to the screen 
+
+  // Print Array Contents -- All Elements on Single Line
+  Console.WriteLine("The elements of each array are:\n"); 
+  Console.WriteLine("breakFastFoods: \n" + String.Join("\n", breakfastFoods));
+  Console.WriteLine(); // Print an empty line to the screen
+
+  // How long is my array? BETERNIMING ARRAY LENGTH
+  Console.WriteLine("The length of the breakFastFoods array is: " + breakfastFoods.Length);
+  // Length is known as a PROPERTY of the array.
+
+  // Accesing Elements in the array with the index
+  Console.WriteLine("The first element of the breakFastFoods array is: " + breakfastFoods[0];);
+  // PRINT THE 2nd ELEMENT OF THE testScores array
+  // PRINT THE 3rd ELEMENT OF THE GPA array
+
 
 
 
