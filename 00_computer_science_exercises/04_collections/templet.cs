665296bb862b143ab72065fd55cfe40a2f2af7bb
semaiaa <maialei06@icloud.com>
2024-10-30 12:38:30 -0400

    Create templet.cs

---
+++ 00_computer_science_exercises/04_collections/templet.cs
@@ -0,0 +1,35 @@
+//Data Types and Operators, Semaia Encalade, v0.0
+using System; 
+using System.Ling; 
+using System.Collections;
+
+
+using System;
+class T04_Collections {
+  static void Main() { // DO NOT DELETE
+  /*
+  In C# a collection is known as an ARRAY. 
+  -- EACH ITEM IN THE ARRAY IS AN ELEMENT
+  -- NUMBER OF ELEMENTS IN THE ARRAY CANNOT BE CHANGED
+  -- CONTENTS OF EACH ELEMENT CAN CHANGE
+  -- ALWAYS ARE ORDERED, MEANING EACH ITEM HAS A FIXED POSITION
+  -- POSITION OF THE ELEMENT IS THE INDEX
+  -- FIRST ELEMENT INDEX IS 0
+  */
+
+  // DECLARING AND DEFINING AN ARRAY
+  string[] breakfastFoods = {"Bacon", "Waffals", "Sausage", "Eggs" "Grits"};
+  int[] testScore = {0, 25, 15, 75, 99};
+  double[]GPA = {0.27D, 1,99D, 4.02D, 0.0D, 3.25D};
+  
+
+
+
+
+
+
+  } // DO NOT DELETE EVER, SHOULD BE 3 SPACES INDENTED FROM THE LEFT
+} // DO NOT DELETE EVER, SHOULD ALWAYS TOUCH THE LEFT MARGIN 
+  
+
+    
